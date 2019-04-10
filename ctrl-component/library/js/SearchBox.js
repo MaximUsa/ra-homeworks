5e173f7bb4d0b1d18baccc142b4d558cc5a409a8
Maxim Usanov <max.usanov@gmail.com>
2019-04-10 17:59:02 +0300

    code added

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,15 @@
-const SearchBox = () => {
+const SearchBox = (props) => {
+
+    const filter = event => {
+        props.filterBooks(event.currentTarget.value);
+    };
+
     return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
+        <input
+            type="text"
+            placeholder="Введите название или автора"
+            onChange={filter}
+            value={props.value}
+        />
     );
 };
