540c28f189ab0562c4d621b0d47ba1fafe65d3c7
Maxim Usanov <max.usanov@gmail.com>
2019-04-16 17:23:30 +0300

    done

--- hoc/table-aggregation/js/App.js
+++ hoc/table-aggregation/js/App.js
@@ -1,5 +1,78 @@
 'use strict';
 
+const tableWrapper = (Component, tableType) => {
+    return class WrappedTable extends React.Component {
+        constructor(props) {
+            super(props);
+            this.convertedList = [];
+        }
+
+        getYear = (date = new Date()) => new Date(date).getFullYear();
+
+        getMonth = (date) => new Date(date).toLocaleString('ru-ru', {month: 'short'});
+
+        amountMonth = (result, item) => {
+            const {date, amount} = item;
+            const dateMonth = this.getMonth(date);
+            const indexMonth = result.findIndex(el => el.month === dateMonth);
+
+            if (indexMonth !== -1) {
+                result[indexMonth].amount += amount;
+            } else {
+                result = [...result, {month: dateMonth, amount}];
+            }
+
+            return result;
+        };
+
+        amountYear = (result, item) => {
+            const {date, amount} = item;
+            const dateYear = this.getYear(date);
+            const indexYear = result.findIndex(el => el.year === dateYear);
+
+            if (indexYear !== -1) {
+                result[indexYear].amount += amount;
+            } else {
+                result = [...result, {year: dateYear, amount}]
+            }
+
+            return result;
+        };
+
+        componentWillUpdate(newProps) {
+            let list = [...newProps.list];
+            if (tableType === 'year') {
+                {
+                    list = list.reduce(this.amountYear, []);
+
+                }
+            } else if (tableType === 'month') {
+                {
+                    list = list.reduce(this.amountMonth, []);
+
+                }
+            } else {
+                {
+                    list.sort((a, b) => a.date < b.date ? 1 : -1);
+                }
+            }
+
+            this.convertedList = list;
+        }
+
+
+        render() {
+            return (
+                <Component {...this.props} list={this.convertedList}/>
+            );
+        }
+    }
+};
+
+const WrapMonthTable = tableWrapper(MonthTable, 'month');
+const WrapYearTable = tableWrapper(YearTable, 'year');
+const WrapSortTable = tableWrapper(SortTable);
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -9,18 +82,19 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        axios.get('https://api.myjson.com/bins/l2s9l').then(response => {
-            this.setState(response.data);
-        });
+        axios.get('https://api.myjson.com/bins/l2s9l')
+            .then(response => {
+                this.setState(response.data);
+            });
     }
 
     render() {
         return (
             <div id="app">
-                <MonthTable list={this.state.list} />
-                <YearTable list={this.state.list} />
-                <SortTable list={this.state.list} />
+                <WrapMonthTable list={this.state.list}/>
+                <WrapYearTable list={this.state.list}/>
+                <WrapSortTable list={this.state.list}/>
             </div>
         );
     }
-};
+}
