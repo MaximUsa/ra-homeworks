10df53f8e9df6f0a2376d98f082ae693c196febc
Maxim Usanov <max.usanov@gmail.com>
2019-04-19 18:22:15 +0300

    done

--- routing-xhr/logs/js/App.js
+++ routing-xhr/logs/js/App.js
@@ -1,41 +1,76 @@
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      logs: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            logs: []
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://baconipsum.com/api/?type=meat-and-filler&paras=50')
+            .then(response => response.json())
+            .then(logs => this.setState({logs}));
+    }
+
+    render() {
+        const {logs} = this.state;
+
+        return (
+            <Router>
+                <div>
+                    <ul className="nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Текущие данные</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/archive">Архив</Link>
+                        </li>
+                    </ul>
+                    <Switch>
+                        <Route exact path="/">
+                            <Current logs={logs}/>
+                        </Route>
+                        <Route path="/archive">
+                            <Archive logs={logs}/>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
+
+const withFetcher = ({url, collName}) => WrappedComponent => class extends React.Component {
+    state = {
+        [collName]: []
     };
-  }
 
-  componentDidMount() {
-    fetch('https://baconipsum.com/api/?type=meat-and-filler&paras=50')
-      .then(response => response.json())
-      .then(logs => this.setState({ logs }));
-  }
+    componentWillMount() {
 
-  render() {
-    const { logs } = this.state;
+        if (typeof url === 'function') {
+            url = url(this.props);
+        }
+        const params = {
+            method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        };
+        fetch(url, params)
+            .then(response => response.json())
+            .then(result => this.setState({[collName]: result}));
+    }
 
-    return (
-      <Router>
-        <div>
-          <ul className="nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Текущие данные</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/archive">Архив</Link>
-            </li>
-          </ul>
-          <Switch>
-            <Route exact path="/">
-              <Current logs={logs} />
-            </Route>
-            <Route path="/archive">
-              <Archive logs={logs} />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+
+    render() {
+        return (
+            <WrappedComponent {...this.props} {...this.state} />
+        );
+    }
 };
+
+
+const Main = withFetcher({
+    url: 'https://baconipsum.com/api/?type=meat-and-filler&paras=50',
+    collName: 'logs'
+})(App);
