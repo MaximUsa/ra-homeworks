bdcf91596a16dd819c43952a03988f06a360446f
Maxim Usanov <max.usanov@gmail.com>
2019-04-22 22:17:28 +0300

    fix

--- instruments/feedback/js/Form.js
+++ instruments/feedback/js/Form.js
@@ -1,70 +1,69 @@
 class Form extends React.Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      name: '',
-      text: '',
-      done: false,
+        this.state = {
+            name: '',
+            text: '',
+            done: false,
+        };
+
+        this.send = this.send.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this)
     }
 
-    this.send = this.send.bind(this)
-    this.handleNameChange = this.handleNameChange.bind(this)
-    this.handleTextChange = this.handleTextChange.bind(this)
-  }
+    send() {
+        this.setState(
+            {done: true},
+            this.handleForm,
+        )
+    }
 
-  send() {
-    this.setState(
-      { done: true },
-      this.handleForm,
-    )
-  }
+    handleForm() {
+        const {name, text, done} = this.state;
+        console.log(name, text);
+        setTimeout(() => {
+            this.setState({done: !done})
+        }, 1000)
+    }
 
-  handleForm() {
-    setTimeout(function() {
-      const { name, text } = this.state
-      console.log(name, text)
+    handleNameChange(e) {
+        this.setState({name: e.target.value})
+    }
 
-      this.setState({ done: false })
-    }, 1000)
-  }
+    handleTextChange(e) {
+        this.setState({text: e.target.value})
+    }
 
-  handleNameChange(e) {
-    this.setState({ name: e.target.value })
-  }
+    render() {
+        const {done} = this.state;
 
-  handleTextChange(e) {
-    this.setState({ text: e.target.value })
-  }
+        return (
+            <main className="container">
+                <div className="row">
+                    <section className="col-ls-6 offset-lg-3 col-sm-12">
+                        <div className="form">
+                            <h2>Обратная связь</h2>
+                            <p>Нам важно ваше мнение</p>
 
-  render() {
-    const { done } = this.state
+                            <label>
+                                Имя
+                                <input className="input" onChange={this.handleNameChange}/>
+                            </label>
 
-    console.log(this.state)
+                            <label>
+                                Сообщение
+                                <textarea className="input" onChange={this.handleTextChange}/>
+                            </label>
 
-    return (
-      <main className="container">
-        <div className="row">
-          <section className="col-ls-6 offset-lg-3 col-sm-12">
-            <div className="form">
-              <h2>Обратная связь</h2>
-              <p>Нам важно ваше мнение</p>
-
-              <label>
-                Имя
-                <input className="input" onChange={this.handleNameChange} />
-              </label>
-
-              <label>
-                Сообщение
-                <textarea className="input" onChange={this.handleTextChange}></textarea>
-              </label>
-
-              <button className={`form__send ${done && 'form__send-disabled'}`} onClick={this.send}>Отправить</button>
-            </div>
-          </section>
-        </div>
-      </main>
-    );
-  }
+                            <button className={`form__send ${done && 'form__send-disabled'}`}
+                                    onClick={this.send}>Отправить
+                            </button>
+                        </div>
+                    </section>
+                </div>
+            </main>
+        );
+    }
 }
