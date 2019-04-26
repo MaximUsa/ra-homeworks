57ec61222d8fd949a60f2aeebe4256d1820ef6c8
Maxim Usanov <max.usanov@gmail.com>
2019-04-26 15:59:37 +0300

    done

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const AuthForm = onAuth => {
+
+    const auth = (event) => {
+        event.preventDefault();
+        if ((!onAuth) || (typeof onAuth !== 'function')) return null;
+        const elementFrom = event.currentTarget.elements;
+        const authForm = {
+            name: elementFrom.name.value,
+            email: elementFrom.email.value,
+            password: elementFrom.password.value
+        };
+        onAuth(authForm);
+    };
+
+    function regExEmail(event) {
+        return event.currentTarget.value = event.currentTarget.value.replace(/[^\w@.]+/gi, '');
+    }
+
+    function regExPassword(event) {
+        return event.currentTarget.value = event.currentTarget.value.replace(/[^\w]+/gi, '')
+    }
+
+    return (
+        <form className="ModalForm" onSubmit={auth} action="/404/auth/" method="POST">
+            <div className="Input">
+                <input required type="text" placeholder="Имя" name='name'/>
+                <label> </label>
+            </div>
+            <div className="Input">
+                <input type="email" onChange={regExEmail} placeholder="Электронная почта" name='email'/>
+                <label> </label>
+            </div>
+            <div className="Input">
+                <input required type="password" onChange={regExPassword} placeholder="Пароль" name='password'/>
+                <label> </label>
+            </div>
+            <button type="submit">
+                <span>Войти</span>
+                <i className="fa fa-fw fa-chevron-right"/>
+            </button>
+        </form>
+    );
+
+};
